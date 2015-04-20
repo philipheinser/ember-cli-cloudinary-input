f8b2b9afcd475cc577e49cb615c37e1a0a499f74
Philip Heinser <philipheinser@me.com>
2015-04-20 13:00:53 +0200

    added upload input

---
+++ addon/components/cloudinary-upload.js
@@ -0,0 +1,34 @@
+import Ember from 'ember';
+import config from '../config/environment';
+
+export default Ember.Component.extend({
+  tagName: 'input',
+  name: 'file',
+  classNames: ['cloudinary-fileupload'],
+  attributeBindings: ['name', 'type', 'accept'],
+  type: 'file',
+  accept: null,
+  cloudinaryImage: null,
+
+  setupUpload: function() {
+    var _this = this;
+    var controller = this.get('controller');
+
+    this.$().unsigned_cloudinary_upload(
+      config.CLOUDINARY_UPLOAD_PRESET, {
+        cloud_name: config.CLOUDINARY_NAME
+      }, {
+        disableImageResize: false,
+        imageMaxWidth: 1000,
+        imageMaxHeight: 1000,
+        acceptFileTypes: /(\.|\/)(gif|jpe?g|png|bmp|ico)$/i,
+        maxFileSize: 5000000 // 5MB
+      }
+    );
+
+    this.$().bind('fileuploaddone', function (e, data) {
+      console.log(data.result)
+      _this.set('cloudinaryImage', data.result);
+    });
+  }.on('didInsertElement')
+});
